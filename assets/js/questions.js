2db08cf73b95e4175aaeb74d0b9a2b2d5f3f1840
seunbayo <seunbayo83@gmail.com>
2020-06-03 00:08:59 +0100

    added some js file and some work

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,61 +5,60 @@ var questions = [{
     "option3": "Mark zuckerberg",
     "option4": "Charles babbage",
     "answer":  "4"
-},{
+}, {
     "question": "Who of the following is the most powerful type of computer?",
     "option1": "Megaframe",
     "option2": "super computer",
     "option3": "super conductor",
     "option4": "Laptop",
     "answer":  "2"
-},{
+}, {
     "question": "C is ?",
     "option1": "A machine language",
     "option2": "All of the above",
     "option3": "An assembly language",
     "option4": "A third generation high level language",
     "answer":  "4"
-},{
+}, {
     "question": "Web pages are written using?",
     "option1": "HTML",
     "option2": "FRP",
     "option3": "URL",
     "option4": "CSSP",
     "answer":  "1"
-},{
+}, {
     "question": "Which is a single integrated circuit?",
     "option1": "HP",
     "option2": "Chip",
     "option3": "Gate",
     "option4": "CPU",
     "answer":  "3"
-},{
+}, {
     "question": "Find the odd one among the following",
     "option1": "Oracle",
     "option2": "Access",
     "option3": "Informix",
     "option4": "C",
     "answer":  "4"
-},{
+}, {
     "question": "Which of the following is an example of a non volatile memory?",
     "option1": "RAM",
     "option2": "ROM",
     "option3": "GIG",
     "option4": "LZI",
     "answer":  "2"
-},{
+}, {
     "question": "What is the full meaning of CAI?",
     "option1": "computer authorised isue",
     "option2": "computer aided instruction",
     "option3": "computer accessories",
     "option4": "Computer assited information",
     "answer":  "2"
-},{
+}, {
     "question": "What is the full meaning of HP?",
     "option1": "Hewlet packard",
     "option2": "Hewlett packard",
     "option3": "Hewlett packarrd",
     "option4": "Hewlett pakard",
     "answer":  "2"
-},
-]
+},]
